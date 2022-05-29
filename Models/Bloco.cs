106acfa473a5984fb971ce19213d2c96aef11801
DW Rodrigues <denys.w.r@hotmail.com>
2022-05-29 17:08:16 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Bloco.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TCC_Teste.Models
+{
+    public class Bloco
+    {
+        [Key]
+        public Guid IDBloco { get; set; }
+
+        public string NomeBloco { get; set; }
+
+        public int ApartamentoInicial { get; set; }
+
+        public int ApartamentoFinal { get; set; }
+
+        [
+        public Guid IDCondominio { get; set; }
+    }
+}
---
+++ Models/Condominio.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TCC_Teste.Models
+{
+    public class Condominio
+    {
+        [Key]
+        public Guid IDCondominio { get; set; }
+
+        [Required]
+        public string NomeCondominio { get; set; }
+
+        [Required]
+        public string Logradouro { get; set; }
+
+        [Required]
+        public string Bairro { get; set; }
+
+        [Required]
+        public string Cidade { get; set; }
+
+        [Required]
+        public int Numero { get; set; }
+
+        [Required]
+        public string CEP { get; set; }
+
+        [Required]
+        public string UF { get; set; }
+    }
+}
